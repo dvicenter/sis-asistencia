73d7816a673c0a080d402c4827128247d784c028
Diego Vicente <xxdiego669xx@gmail.com>
2015-01-05 18:19:38 -0500

    login

---
+++ application/views/v_login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Acceso Asistencia</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="<?php echo base_url();?>resource/css/base.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>resource/css/bootstrap.min.css">
+	<script src="<?php echo base_url();?>resource/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<section id="sec-login">
+		<form>
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Email address</label>
+		    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputPassword1">Password</label>
+		    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		  </div>
+		  <button type="submit" class="btn btn-default">Submit</button>
+		</form>
+	</section>
+</body>
+</html>
